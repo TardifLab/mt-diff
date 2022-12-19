32f5a4e5bac149c0968a768e5ad77a05ad030585
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2022-12-19 14:27:45 -0500

    using joint filtering of the streamline weights

--- batch-process.sh
+++ batch-process.sh
@@ -9,21 +9,20 @@
 #$ -V
 
 #source /data_/tardiflab/pbontempi/scripts/init.sh
-aa2_preprocessing_samebias.sh hc18r
+#aa2_preprocessing_samebias.sh hc23r
 
-bb1_Script_05_iFOD_ss3t_tractography_upsampling_as_ISMRM.sh hc18r
-cc1_confidence_map.sh hc18r
+#bb1_Script_05_iFOD_ss3t_tractography_upsampling_as_ISMRM.sh hc23r
+#cc1_confidence_map.sh hc23r
 
-source /data_/tardiflab/pbontempi/scripts/dd1_enable_python_venv.sh
-python3 dd2_COMMIT_MToff_as_ISMRM.py hc18r
-python3 dd3_COMMIT_MTon_as_ISMRM.py hc18r
+#source /data_/tardiflab/pbontempi/scripts/dd1_enable_python_venv.sh
+#python3 dd2_COMMIT_MToff_as_ISMRM.py hc23r
+#python3 dd3_COMMIT_MTon_as_ISMRM.py hc23r
 
-python3 ee1_connectomes_MToff.py hc18r
-python3 ee2_connectomes_MTon.py hc18r
+#python3 ee1_connectomes_MToff_jointFilter.py hc23r
+#python3 ee2_connectomes_MTon_jointFilter.py hc23r
 
-python3 ff1_MTRconnectome.py hc18r
+#python3 ff1_MTRconnectome.py hc23r
 
-## These can be run in parallel
+## These can be run in parallel and are for visualization, don't need to run for everyone
 python3 gg1_MTR_bundle.py hc18r
 python3 gg2_MTRdw_bundle.py hc18r
-
